e226a1bb8cfa9eb4b56f057d2384a4b5461d7672
Kysha Mae R. <kyshamae.rocabo@evsu.edu.ph>
2025-09-19 15:28:26 +0800

    Remove core and joashproject folders

--- core/views.py
+++
@@ -1,8 +0,0 @@
-# myapp/views.py
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "myapp/home.html")
-
-def about(request):
-    return render(request, "myapp/about.html")
